ce228230067d313499a73b7e697c7019b5fac266
Heron Yang <heron.yang.tw@gmail.com>
2016-11-01 10:56:35 -0400

    update

--- upload.sh
+++ upload.sh
@@ -3,6 +3,6 @@ FILES=./data.zip.*
 for f in $FILES
 do
     echo "Uploading $f file..."
-    # rsync --progress -rave "ssh -i ~/17-648/doc/1/cmu-17648-linux.pem" $f ec2-user@ec2-54-196-236-65.compute-1.amazonaws.com:~/data/ > ./log/$f.log &
+    # rsync --progress -rave "ssh -i ~/17-648/doc/1/cmu-17648-linux.pem" $f ec2-user@ec2-xxx-xxx-xxx-xxx.compute-1.amazonaws.com:~/data/ > ./log/$f.log &
     rsync --progress -rave "ssh -i MY_MACHINE_KEY.pem" $f ec2-user@MY_MACHINE_URL:~/data/ > ./log/$f.log &
 done
